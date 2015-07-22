019e9827a0c12e3d02b2a9e5d8e7070e50134a30
Ramsey McGrath <RamseyMcGrath@gmail.com>
2015-07-22 14:31:28 -0700

    Initial Commit

---
+++ manifests/init.pp
@@ -0,0 +1,27 @@
+# Disables huge page deamon, and alters grub.conf to keep it disabled
+#
+# 
+#
+class remove_hugepage {
+
+  include augeas
+
+  augeas { 'grub.conf/no_khugepaged':
+      incl    => '/etc/grub.conf',
+      lens    => 'grub.lns',
+      changes => [
+          'setm title[*]/kernel/ transparent_hugepage never',
+      ],
+  }
+
+  exec { 'disable_transparent_hugepage_enabled':
+    command => '/bin/echo never > /sys/kernel/mm/transparent_hugepage/enabled',
+    unless  => '/bin/grep -c \'\\[never\\]\' /sys/kernel/mm/transparent_hugepage/enabled 2>/dev/null',
+  }
+
+  exec { 'disable_transparent_hugepage_defrag':
+    command => '/bin/echo never > /sys/kernel/mm/transparent_hugepage/defrag',
+    unless  => '/bin/grep -c \'\\[never\\]\' /sys/kernel/mm/transparent_hugepage/defrag 2>/dev/null',
+  }
+
+}
